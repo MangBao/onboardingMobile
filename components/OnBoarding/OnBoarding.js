5175e26a8e0a070583ae7224100504403eef01f1
MangBao <mangbao1301@gmail.com>
2022-11-20 18:20:01 +0700

    #add flex

--- components/OnBoarding/OnBoarding.js
+++ components/OnBoarding/OnBoarding.js
@@ -1,18 +1,36 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {View, FlatList} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, FlatList, StyleSheet, Text, Animated} from 'react-native';
 import slides from '../../slides';
 import OnBoardingItem from './OnBoardingItem';
 
 export default function OnBoarding() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const viewableItemsChanged = useRef(({viewableItems}) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  // const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
   return (
     <View>
       <FlatList
         data={slides}
         renderItem={({item}) => <OnBoardingItem item={item} />}
-        // horizontal
-        // showsHorizontalScrollIndicator
-        // pagingEnabled
-        // bounces={false}
+        horizontal
+        showsHorizontalScrollIndicator
+        pagingEnabled
+        bounces={false}
+        keyExtractor={item => item.id}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffet: {x: scrollX}}}],
+          {
+            useNativeDriver: false,
+          },
+        )}
+        onViewableItemsChanged={viewableItemsChanged}
       />
     </View>
   );
--- components/OnBoarding/OnBoardingItem.js
+++ components/OnBoarding/OnBoardingItem.js
@@ -27,5 +27,13 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 0.7,
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: '800',
+    fontSize: 28,
+    marginBottom: 10,
+    color: 'black',
+    textAlign: 'center',
   },
 });
